fdbc9749a20ea0f23651233751a920c35c3ec1fc
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-16 15:46:39 +0300

    Ready

---
+++ Ex41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int Check(int[] array)
+{   
+    int count = 0;
+    int[] mas = array;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void FillArray(int[] array)
+{   
+    for (int i = 0; i < array.Length; i++)
+    {   
+        Console.WriteLine("Введите элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int M = 8;
+int[] mas = new int[M];
+FillArray(mas);
+PrintArray(mas);
+System.Console.WriteLine();
+int res = Check(mas);
+System.Console.WriteLine(res);
+
---
+++ Ex45/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double[] array = {b1,k1,b2,k2};
+
+string TochkaPeresechenia(double[] array)
+{
+    
+    double x = (array[2]-array[0])/(array[1]-array[3]);
+    double y1 = array[1] * x + array[0];
+    double y2 = array[3] * x + array[2];
+    string res = $"({y1} ; {y2})";
+    return res;
+}
+
+string res = TochkaPeresechenia(array);
+System.Console.WriteLine($"{res}");
+
